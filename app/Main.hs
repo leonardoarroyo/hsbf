2ebfa2c81bacbe56ba4d366fde49240fadab1c8d
Leonardo Arroyo <contato@leonardoarroyo.com>
2021-07-07 01:45:02 -0300

    Naive implementation for >, <, + and -

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,59 @@
 module Main where
 
+import Control.Lens (element, (&), (.~))
 import Control.Monad.State
+import Data.Sequence
+import Debug.Trace
 
-data Stmt = Plus | Minus | GT | LT | In | Out | LoopStart | LoopEnd
+data Stmt = Increment | Decrement | MoveRight | MoveLeft deriving (Show)
 
-data Prog
-  = Seq [Stmt]
-  | Loop Prog
+type Prog = Seq Stmt
+
+type ProgramValue = Int
+
+type Tape = [Int]
 
 data ProgramState = ProgramState
-  { tape :: [Int],
+  { tape :: Tape,
     ptr :: Int
   }
+  deriving (Show)
 
---main = print $ evalState (playGame "abcaaacbbcabbab") startState
+type ProgramStateM = State ProgramState
+
+newState :: Int -> ProgramState
+newState memorySize = ProgramState [0 | i <- [0 .. memorySize -1]] 0
+
+processStatement :: Stmt -> ProgramState -> ProgramState
+processStatement stmt state =
+  case stmt of
+    Increment -> ProgramState (tape' & element ptr' .~ ((tape' !! ptr') + 1)) ptr'
+    Decrement -> ProgramState (tape' & element ptr' .~ ((tape' !! ptr') - 1)) ptr'
+    MoveRight -> ProgramState tape' (ptr' + 1)
+    MoveLeft -> ProgramState tape' (ptr' - 1)
+  where
+    tape' = tape state
+    ptr' = ptr state
+
+runStatement :: Stmt -> ProgramStateM ()
+runStatement stmt = do
+  modify (processStatement stmt)
+
+evalProgram :: Prog -> ProgramStateM ProgramState
+evalProgram prog | trace ("evalProgram " ++ show prog) False = undefined
+evalProgram prog = do
+  runStatement stmt
+  if hasMore then evalProgram tail else get
+  get
+  where
+    stmt = Data.Sequence.index prog 0
+    tail = Data.Sequence.drop 1 prog
+    hasMore = Data.Sequence.length tail > 0
+
+main :: IO ()
+main =
+  do
+    print $ execState (evalProgram basicProgram) zero
+  where
+    basicProgram = fromList [Decrement, MoveRight, Increment, MoveRight, MoveRight, Increment, Increment]
+    zero = newState 1000
