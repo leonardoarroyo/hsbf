6146d30c754c7f52f0faf311f929b88348734589
Leonardo Arroyo <contato@leonardoarroyo.com>
2021-07-24 07:57:42 -0300

    add awkward implementation for loops

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,7 @@
 -- Implement CLI
 -- Implement compiler
 -- Implement tests
+-- CI/CD
 module Main where
 
 import Control.DeepSeq
@@ -31,6 +32,7 @@ data Stmt
   | MoveLeft
   | CharIn
   | CharOut
+  | Loop [Stmt]
   | Exit
   deriving (Show, Eq)
 
@@ -45,7 +47,8 @@ data Status = Running | Exited deriving (Eq)
 data ProgramState = ProgramState
   { tape :: Tape,
     ptr :: Int,
-    prog :: Prog
+    prog :: Prog,
+    prog_stack :: [Prog]
   }
   deriving (Show, Eq)
 
@@ -56,14 +59,15 @@ advance (x : xs) = xs
 newPState :: ProgramState
 newPState =
   ProgramState
-    [100 | i <- [0 .. 100]]
+    [0 | i <- [0 .. 100]]
     0
-    [CharIn, MoveRight, CharIn, MoveLeft, CharOut, MoveRight, CharOut]
+    [Increment, Increment, Increment, Loop [Decrement, MoveRight, Increment, Increment, Loop [Decrement, MoveRight, Increment, Increment, MoveLeft], MoveLeft]]
+    []
 
 increment :: StateT ProgramState IO Status
 increment = state f
   where
-    f xs = (Running, ProgramState newTape (ptr xs) (prog xs))
+    f xs = (Running, ProgramState newTape (ptr xs) (prog xs) (prog_stack xs))
       where
         actualTape = tape xs
         pointer = ptr xs
@@ -72,17 +76,17 @@ increment = state f
 decrement :: StateT ProgramState IO Status
 decrement = state f
   where
-    f xs = (Running, ProgramState newTape (ptr xs) (prog xs))
+    f xs = (Running, ProgramState newTape (ptr xs) (prog xs) (prog_stack xs))
       where
         actualTape = tape xs
         pointer = ptr xs
         newTape = actualTape & element pointer .~ ((actualTape !! pointer) - 1)
 
 moveRight :: StateT ProgramState IO Status
-moveRight = state (\xs -> (Running, ProgramState (tape xs) (ptr xs + 1) (prog xs)))
+moveRight = state (\xs -> (Running, ProgramState (tape xs) (ptr xs + 1) (prog xs) (prog_stack xs)))
 
 moveLeft :: StateT ProgramState IO Status
-moveLeft = state (\xs -> (Running, ProgramState (tape xs) (ptr xs - 1) (prog xs)))
+moveLeft = state (\xs -> (Running, ProgramState (tape xs) (ptr xs - 1) (prog xs) (prog_stack xs)))
 
 charOut :: StateT ProgramState IO Status
 charOut = do
@@ -95,7 +99,7 @@ charIn = do
   char <- io getChar
   state $ f char
   where
-    f char s = (Running, ProgramState newTape (ptr s) (prog s))
+    f char s = (Running, ProgramState newTape (ptr s) (prog s) (prog_stack s))
       where
         actualTape = tape s
         pointer = ptr s
@@ -106,26 +110,40 @@ popInstruction = do
   st' <- get
   state f
   where
-    f s = (headStatement prog', ProgramState (tape s) (ptr s) newProg)
+    f s = (headStatement prog', ProgramState (tape s) (ptr s) newProg (prog_stack s))
       where
         prog' = prog s
         headStatement (x : xs) = x
         headStatement [] = Exit
         newProg = drop 1 prog'
 
-peekInstruction :: StateT ProgramState IO Stmt
-peekInstruction = do
-  st' <- get
-  state f
-  where
-    f s = (headStatement prog', s)
-      where
-        prog' = prog s
-        headStatement (x : xs) = x
-        headStatement [] = Exit
-
 exit :: StateT ProgramState IO Status
 exit = state (\s -> (Exited, s))
+
+loop :: [Stmt] -> StateT ProgramState IO Status
+loop stmts = do
+  st <- get
+  put $ stack_program st
+  st <- get
+  put $ load_program st stmts
+  iterateWhile (== Running) popAndRun
+  st <- get
+  put $ pop_stack st
+  st <- get
+  if tape st !! ptr st == 0
+    then put st
+    else put $ restore_loop st stmts
+  return Running
+  where
+    f s = (Running, ProgramState (tape s) (ptr s) (prog s) (prog_stack s))
+    stack_program st = ProgramState (tape st) (ptr st) [] (prog st : prog_stack st)
+    load_program st stmts = ProgramState (tape st) (ptr st) stmts (prog_stack st)
+    restore_loop st stmts = ProgramState (tape st) (ptr st) (Loop stmts : prog st) (prog_stack st)
+    pop_stack st = ProgramState (tape st) (ptr st) (stack_head (prog_stack st)) (stack_tail (prog_stack st))
+      where
+        stack_head (x : xs) = x
+        stack_head [] = []
+        stack_tail = drop 1
 
 executeStatement :: Stmt -> StateT ProgramState IO Status
 executeStatement stmt = case stmt of
@@ -135,16 +153,15 @@ executeStatement stmt = case stmt of
   Decrement -> decrement
   CharIn -> charIn
   CharOut -> charOut
+  Loop stmts -> loop stmts
   Exit -> exit
 
 popAndRun :: StateT ProgramState IO Status
-popAndRun = do
-  instruction <- popInstruction
-  executeStatement instruction
+popAndRun = popInstruction >>= executeStatement
 
 runTestPrg :: StateT ProgramState IO ()
 runTestPrg = do
-  iterateWhile (\a -> a == Running) popAndRun
+  iterateWhile (== Running) popAndRun
   st <- get
   io $ print st
   return ()
@@ -156,58 +173,3 @@ main = do
 
 io :: IO a -> StateT ProgramState IO a
 io = liftIO
-
--- --------------------------
--- --------------------------
--- newState :: Int -> ProgramState
--- newState memorySize = ProgramState [0 | i <- [0 .. memorySize -1]] 0
---
--- processStatement :: Stmt -> ProgramState -> ProgramState
--- processStatement stmt state =
---   case stmt of
---     Increment -> ProgramState (tape' & element ptr' .~ ((tape' !! ptr') + 1)) ptr'
---     Decrement -> ProgramState (tape' & element ptr' .~ ((tape' !! ptr') - 1)) ptr'
---     MoveRight -> ProgramState tape' (ptr' + 1)
---     MoveLeft -> ProgramState tape' (ptr' - 1)
---   where
---     tape' = tape state
---     ptr' = ptr state
---
--- runStatement :: Stmt -> ProgramStateM ()
--- runStatement stmt = do
---   modify (processStatement stmt)
---
--- evalProgram :: Prog -> ProgramStateM ProgramState
--- evalProgram prog | trace ("evalProgram \n" ++ show stmt ++ "|" ++ show tail ++ "\n" ++ show hasMore ++ "\n") False = undefined
---   where
---     stmt = if Data.Sequence.length prog > 0 then Just (Data.Sequence.index prog 0) else Nothing
---     tail = Data.Sequence.drop 1 prog
---     hasMore = Data.Sequence.length tail > 0
--- evalProgram prog =
---   get >>= \st ->
---     case stmt of
---       Just stmt -> case stmt of
---         Loop a -> if tape st !! ptr st == 0 then get else evalProgram a `seq` evalProgram prog
---         _ -> runStatement stmt >> get
---       Nothing -> get
---       `seq` if hasMore then evalProgram tail else get
---   where
---     stmt = if Data.Sequence.length prog > 0 then Just (Data.Sequence.index prog 0) else Nothing
---     tail = Data.Sequence.drop 1 prog
---     hasMore = Data.Sequence.length tail > 0
---
--- strict :: (t -> b) -> t -> b
--- strict f x = seq x (f x)
-
---main :: IO ()
---main =
---  do
---    print $ execState (evalProgram basicProgram) zero
---  where
---    basicProgram = fromList [Increment, Increment, Increment, Loop loopInner]
---    loopInner = strict fromList [Decrement, MoveRight, Increment, MoveRight, Increment, Increment, Increment, MoveLeft, MoveLeft] --, Loop innerMost]
---    innerMost = strict fromList [Decrement, MoveRight, Increment, MoveLeft]
---    zero = newState 1000
-
-------------------------
---
